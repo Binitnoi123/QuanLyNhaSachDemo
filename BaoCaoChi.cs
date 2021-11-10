3b73eb06f632a398efb50f8b64be96546272a22e
Binitnoi123 <94070912+Binitnoi123@users.noreply.github.com>
2021-11-11 06:02:31 +0700

    Đây là file code của nhóm mình

---
+++ BaoCaoChi.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Reporting.WinForms;
+using QuanLyNhaSach.DataBase;
+
+namespace QuanLyNhaSach
+{
+    public partial class BaoCaoChi : Form
+    {
+        Model1 DbContext;
+        public BaoCaoChi()
+        {
+            InitializeComponent();
+            DbContext = new Model1();
+        }
+
+        private void BaoCaoChi_Load(object sender, EventArgs e)
+        {
+
+            this.reportViewer1.LocalReport.ReportPath = "Report1.rdlc";
+            var reportDataSource = new ReportDataSource("DataSet1", DbContext.PhieuNhaps.ToList());
+            this.reportViewer1.LocalReport.DataSources.Clear(); //clear
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource);
+            this.reportViewer1.RefreshReport(); //ch y report
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
---
+++ BaoCaoThu.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Reporting.WinForms;
+using QuanLyNhaSach.DataBase;
+
+namespace QuanLyNhaSach
+{
+    public partial class BaoCaoThu : Form
+    {
+        Model1 DbContext;
+        public BaoCaoThu()
+        {
+            InitializeComponent();
+            DbContext = new Model1();
+        }
+
+        private void BaoCaoThu_Load(object sender, EventArgs e)
+        {
+            this.reportViewer1.LocalReport.ReportPath = "Report2(Thu).rdlc";
+            var reportDataSource = new ReportDataSource("DataSet1", DbContext.HoaDons.ToList());
+            this.reportViewer1.LocalReport.DataSources.Clear(); //clear
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource);
+            this.reportViewer1.RefreshReport(); //ch y report
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
